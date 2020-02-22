d85f43f1da08afc9baac6cad7aa8695becec53d1
Arlington <arlington.vargas4@gmail.com>
2020-02-22 12:19:56 -0500

    First commit

---
+++ BL/Common/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL.Common
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ ET/ProductET/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET.ProductET
+{
+    public class Product
+    {
+        public int PRId { get; set; }
+        public string PRName { get; set; }
+        public string PRDescription { get; set; }
+        public string PRCode { get; set; }
+        public DateTime PRCreated { get; set; }
+        public DateTime PRModified { get; set; }
+    }
+
+
+}
